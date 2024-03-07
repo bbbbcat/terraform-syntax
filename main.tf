61d5709454ea3439a32cbd507486b3cae08a547b
bbbbcat <bluebluebee86@naver.com>
2024-03-07 11:10:56 +0900

    Create subnet

--- main.tf
+++ main.tf
@@ -16,6 +16,41 @@ provider "aws" {
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "default"
+    Name = "main"
+  }
+}
+
+# create a Public Subnet
+resource "aws_subnet" "Public_subnet_1" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.101.0/24"
+
+  tags = {
+    Name = "Hangramit_public_subnet_1"
+  }
+}
+resource "aws_subnet" "Public_subnet_2" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.102.0/24"
+
+  tags = {
+    Name = "Hangramit_public_subnet_2"
+  }
+}
+# create a private Subnet
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.110.0/24"
+
+  tags = {
+    Name = "Hangramit_private_subnet_1"
+  }
+}
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.111.0/24"
+
+  tags = {
+    Name = "Hangramit_private_subnet_2"
   }
 }
