a81051add1e4f3c7feede85a604f4dc469b41a37
bbbbcat <bluebluebee86@naver.com>
2024-03-07 14:19:52 +0900

    작업완료

--- main.tf
+++ main.tf
@@ -12,31 +12,13 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# create a VPC
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main"
-  }
+# 개발환경
+module "default_custom_vpc" {
+  source = "./custom_vpc"
 }
 
-# create a Public Subnet
-resource "aws_subnet" "Public_subnet_1" {
-  vpc_id            = aws_vpc.default.id
-  cidr_block        = "10.0.100.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "Hangramit_public_subnet_1"
-  }
-}
-resource "aws_subnet" "Hangramit_private_subnet_1" {
-  vpc_id            = aws_vpc.default.id
-  cidr_block        = "10.0.101.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "Hangramit_private_subnet_1"
-  }
+# 운영환경
+module "prd_custom_vpc" {
+  source = "./custom_vpc"
 }
 
