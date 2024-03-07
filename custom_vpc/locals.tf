2a1f672c9eb7712fe3980b4a4a457788c6db801c
bbbbcat <bluebluebee86@naver.com>
2024-03-07 15:51:09 +0900

    변수 처리 완료

---
+++ custom_vpc/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  az_a = "us-west-2a"
+  az_c = "us-west-2c"
+}
--- custom_vpc/main.tf
+++ custom_vpc/main.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "main"
+    Name = "hangramit_vpc_${var.env}"
   }
 }
 
@@ -10,27 +10,33 @@ resource "aws_vpc" "default" {
 resource "aws_subnet" "Public_subnet_1" {
   vpc_id            = aws_vpc.default.id
   cidr_block        = "10.0.100.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = local.az_a
 
   tags = {
-    Name = "Hangramit_public_subnet_1"
+    Name = "Hangramit_public_subnet_1_${var.env}"
   }
 }
 resource "aws_subnet" "Hangramit_private_subnet_1" {
   vpc_id            = aws_vpc.default.id
   cidr_block        = "10.0.102.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = local.az_c
 
   tags = {
-    Name = "Hangramit_private_subnet_1"
+    Name = "Hangramit_private_subnet_1_${var.env}"
   }
 }
 
 resource "aws_nat_gateway" "private_nat_gw" {
   connectivity_type = "private"
   subnet_id         = aws_subnet.Public_subnet_1.id
+  tags = {
+    Name = "hangramit_nat_gw_${var.env}"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "hangaram_igw_${var.env}"
+  }
 }
