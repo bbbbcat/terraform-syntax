54b43447f710b3eca516dee179f86678ad43318b
bbbbcat <bluebluebee86@naver.com>
2024-03-08 10:44:07 +0900

    조건문 작업완료

--- custom_vpc/main.tf
+++ custom_vpc/main.tf
@@ -7,7 +7,10 @@ resource "aws_vpc" "default" {
 }
 
 # create a Public Subnet
+# 퍼블릭서브넷은 dev 일때만 만들어 져야 한다.
 resource "aws_subnet" "Public_subnet_1" {
+  # count 반복문의 특징 0번 반복은 실행이 안된다.
+  count             = var.env == "dev" ? 1 : 0
   vpc_id            = aws_vpc.default.id
   cidr_block        = "10.0.100.0/24"
   availability_zone = local.az_a
@@ -31,4 +34,9 @@ resource "aws_internet_gateway" "igw" {
   tags = {
     Name = "hangaram_igw_${var.env}"
   }
+}
+resource "aws_nat_gateway" "public_net" {
+  count             = var.env == "dev" ? 1 : 0
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.Public_subnet_1[0].id
 }
